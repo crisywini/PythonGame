e493d7ccbb74b19b29f18cf523bb37fe8e9a0443
Cristian <harmaharcri@hotmail.com>
2020-06-10 00:12:23 -0500

    START THE INITIAL LOOP   I start the initial loop of the game Invasión Gatuna

--- SpaceInvaders/main/Main.py
+++ SpaceInvaders/main/Main.py
@@ -5,7 +5,18 @@ Created on 10 jun. 2020
 '''
 import sys
 import pygame
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 700))
+    pygame.display.set_caption('Invasión gatuna')
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+                                    
 def main():
-    print(1)
+    run_game()
 if __name__ == '__main__':
     main()
